493b61fd3a14ba54dbef92647780a79b3ec3f64a
Sebastian <sebastiantrujilloromero23@gmail.com>
2024-10-16 10:08:49 +0200

    i created RegistroActivity

---
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/RegistroActivity1.kt
@@ -0,0 +1,20 @@
+package org.eurekamps.dam2_2425_actividad1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistroActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
