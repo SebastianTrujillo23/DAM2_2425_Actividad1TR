e625383f546e49f9d17a7e8f04ef12c296be7d88
DESKTOP-9RLHA3F\seebb <sebastiantrujilloromero23@gmail.com>
2024-10-17 19:02:08 +0200

    runs after the fragment view

--- app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/SplashFragment.kt
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/SplashFragment.kt
@@ -1,10 +1,13 @@
 package org.eurekamps.dam2_2425_actividad1.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import org.eurekamps.dam2_2425_actividad1.R
 
 
@@ -24,6 +27,16 @@ class SplashFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Simular el tiempo de carga del splash
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Navegar hacia el LoginFragment
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 3000) // 3000 ms = 3 segundos
     }
 
 
