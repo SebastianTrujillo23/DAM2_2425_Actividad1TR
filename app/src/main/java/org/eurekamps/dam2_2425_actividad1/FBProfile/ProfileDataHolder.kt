0c270e48a9d0c4d85311270a371e4401485c286c
Sebastian <sebastiantrujilloromero23@gmail.com>
2024-10-18 12:58:54 +0200

    to save the data

---
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/FBProfile/ProfileDataHolder.kt
@@ -0,0 +1,9 @@
+package org.eurekamps.dam2_2425_actividad1.FBProfile
+
+import org.eurekamps.dam2_2425_actividad1.singletone.Profile
+
+class ProfileDataHolder {
+    companion object {
+        var profile: Profile? = null
+    }
+}
