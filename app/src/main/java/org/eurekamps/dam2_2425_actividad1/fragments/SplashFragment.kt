bbcd28060c5b4a2d8024ed84078e34af7c4534e3
DESKTOP-9RLHA3F\seebb <sebastiantrujilloromero23@gmail.com>
2024-10-18 21:23:28 +0200

    splash modification to meet all requirements

--- app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/SplashFragment.kt
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/SplashFragment.kt
@@ -1,6 +1,5 @@
 package org.eurekamps.dam2_2425_actividad1.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,14 +8,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
-import org.eurekamps.dam2_2425_actividad1.HomeActivity
+import org.eurekamps.dam2_2425_actividad1.FBProfile.ProfileDataHolder
 import org.eurekamps.dam2_2425_actividad1.R
-
 
 class SplashFragment : Fragment() {
 
@@ -24,28 +19,27 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
-override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
 
-    val auth = FirebaseAuth.getInstance()
-    val currentUser = auth.currentUser
-    Log.d("SplashFragment", "Current User: $currentUser") // Agrega esto para verificar
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    Handler(Looper.getMainLooper()).postDelayed({
-        val action = if (currentUser != null) {
-            Log.d("SplashFragment", "User is logged in, navigating to HomeActivity") // Log para verificar
-            R.id.action_splashFragment_to_homeActivity
-        } else {
-            Log.d("SplashFragment", "User is not logged in, navigating to LoginFragment") // Log para verificar
-            R.id.action_splashFragment_to_loginFragment
-        }
-        findNavController().navigate(action)
-    }, 2000) // Esperar 2 segundos
+        val auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+        Log.d("SplashFragment", "Current User: $currentUser")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val action = when {
+                currentUser == null && ProfileDataHolder.profile == null -> R.id.action_splashFragment_to_loginFragment
+
+                currentUser == null && ProfileDataHolder.profile != null -> R.id.action_splashFragment_to_loginFragment
+                currentUser != null && ProfileDataHolder.profile != null -> R.id.action_splashFragment_to_homeActivity
+                else -> R.id.action_splashFragment_to_loginFragment
+            }
+            findNavController().navigate(action)
+        }, 2000)
+    }
 }
 
-
-    }
 
