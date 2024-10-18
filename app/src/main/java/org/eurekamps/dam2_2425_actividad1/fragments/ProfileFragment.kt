bc4468ddad111a2d2f622ccc5087d076f635fb36
Sebastian <sebastiantrujilloromero23@gmail.com>
2024-10-18 09:29:08 +0200

    login fragment checks if there is an identified user

--- app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/ProfileFragment.kt
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/ProfileFragment.kt
@@ -5,27 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import org.eurekamps.dam2_2425_actividad1.R
 
 
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
 
+        // Comprobar si el usuario está logueado
+        if (auth.currentUser == null) {
+            // Mostrar mensaje o redirigir
+            Toast.makeText(context, "Debes estar logueado para acceder a este perfil", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.loginFragment)
+        } else {
+            // Aquí puedes agregar la lógica para mostrar y guardar el perfil
+        }
+    }
 }
